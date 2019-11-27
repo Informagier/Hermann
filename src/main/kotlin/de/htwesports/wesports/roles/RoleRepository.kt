c72ff6e0c5bf0dffc4f84cc80f3aff4b3fea9fb9
Informagier <24705092+Informagier@users.noreply.github.com>
2019-11-27 22:16:55 +0100

    Add RoleRepository

---
+++ src/main/kotlin/de/htwesports/wesports/roles/RoleRepository.kt
@@ -0,0 +1,7 @@
+package de.htwesports.wesports.roles
+
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface RoleRepository : JpaRepository<Role, Long>{
+    fun findByName(name: String) : Role
+}
