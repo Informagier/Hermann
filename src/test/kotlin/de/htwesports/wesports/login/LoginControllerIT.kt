c4ac2a297612d9cee0e0a28b393d07f94d34feb9
Dominik Wendland <dominik.wendland@gmail.com>
2019-11-29 02:29:17 +0100

    Add tests for login page

---
+++ src/test/kotlin/de/htwesports/wesports/login/LoginControllerIT.kt
@@ -0,0 +1,45 @@
+package de.htwesports.wesports.login
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
+import org.springframework.test.web.servlet.post
+
+@SpringBootTest
+@AutoConfigureMockMvc
+internal class LoginControllerIT {
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @Test
+    fun givenLoginPageURI_whenMockMVC_thenReturnsLoginView() {
+        this.mockMvc.get("/login").andDo { print() }.andExpect {
+            status { isOk }
+            view { name("login") }
+        }
+    }
+
+    @Test
+    fun givenFormData_whenLoginSuccess_thenReturnsIndexView() {
+        this.mockMvc.post("/perform_login") {
+            param("username", "user@example.com")
+            param("password", "user")
+        }.andDo { print() }.andExpect {
+            status { isFound }
+        }
+    }
+
+    @Test
+    fun givenFormData_whenLoginFails_thenReturnsLoginView() {
+        this.mockMvc.post("/perform_login") {
+            param("username", "max.mustermann@example.com")
+            param("password", "Test1234")
+        }.andDo { print() }.andExpect {
+            status { isFound }
+        }
+    }
+}
